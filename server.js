0626fa5fc3090549cf44e3f2b45a6285d0950b56
howardwang15 <joy_luck_2000@hotmail.com>
2019-06-22 19:24:20 -0700

    switched order of port assignment smh

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-const port = 8080 || process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log(`Server started on port ${port}!`);
